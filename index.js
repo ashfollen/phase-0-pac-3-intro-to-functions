5aa154e43d92270e1c12243fb646205db740cd65
Ashley Follen <ashleyfollen@outlook.com>
2021-07-06 18:50:20 -0600

    updated work

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Follow along with the examples here
+function doNothing() {}
+
+function sayHello() {
+    console.log('Hello!');
+}
+
+function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+}
+
+function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+}
+
+function sayHelloToBrendan() {
+    console.log("Hello, Brendan!")
+}
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+
+function say(greeting, firstName) {
+    console.log("firstName:", firstName);
+    console.log("greeting:", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+}
+
+function add(x, y) {
+    return x + y;
+}
+
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+}
+
+function say(greeting, firstName) {
+    console.log('I was called!');
+    return `${greeting}, ${firstName}!`;
+}
+
+console.log(say("Howdy", "partner"));
